968f892c0f856cab7a2e4b6ef2dffd155cebde9d
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2023-08-25 01:30:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ import copy
 import os
 from dlisio import dlis
 import seaborn as sns
-
+import sqlite3
+import random
+import string
+import threading
+import time
 
 def inital_plot(tdep_array, fmi_array, well_radius_doi, gt,start,end,min_vug_area,max_vug_area,min_circ_ratio,max_circ_ratio):
         # c_threshold = -1
@@ -350,13 +354,45 @@ def button_clicked(reiterate_button, tdep_array, fmi_array, well_radius_doi, gt,
                     fig1, ax = plt.subplots()
                     sns.histplot(filtered_vugs, ax=ax)
                     st.pyplot(fig1)
- 
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for _ in range(length))
+
 def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(script_dir)
     # st.set_page_config(page_title="Vug Detection", page_icon="🤖", layout="wide", )  
     st.header("Automatic vug detection from FMI logs")
 
+    
+    conn = sqlite3.connect('your_database_name.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS ranges (
+            start REAL,
+            end REAL,
+            status TEXT
+        )
+    ''')
+    def add_to_database(button_name):
+        cursor.execute("INSERT INTO ranges (status) VALUES (?)", (button_name,))
+        conn.commit()
+
+    def clear_database():
+        cursor.execute("DELETE FROM ranges")
+        conn.commit()
+
+    def clear_database_periodically():
+        while True:
+            time.sleep(600)  # Sleep for 10 minutes
+            clear_database()
+
+    # Start the background thread to clear the database
+    clear_thread = threading.Thread(target=clear_database_periodically)
+    clear_thread.daemon = True
+    clear_thread.start()
+    
     uploaded_file = st.file_uploader("Upload DLIS File", type=["dlis"])
     zone_start_selected = 0
     
@@ -374,6 +410,8 @@ def main():
         gt = pd.read_csv("Khufai_Vugs.csv").dropna()[1:].astype('float')
         well_radius_doi = pd.read_csv("well_radius_array.csv")
 
+        
+        
         fmi_array = fmi_df.to_numpy()
         tdep_array = tdep_df .to_numpy()
         well_radius_doi = well_radius_doi.to_numpy()
@@ -448,8 +486,172 @@ def main():
         with col2:
             end = st.number_input("Max Depth", value=default_end)
         
-        inital_plot(tdep_array, fmi_array, well_radius_doi, gt,start,end,min_vug_area,max_vug_area,min_circ_ratio,max_circ_ratio)
+        # c_threshold = -1
+        c_threshold = 'mean'
+        stride_mode = 5
+        k = 5
+        threshold_plotting = False
+        distribution_plot = False
+        area_histogram_plot = False
+        original_contour_plot = True
+        save_plot = True
+        greater_than_mode_percentage_plot = False
+        thrsholding_type = 'adaptive'
+        nearest_point_5 = True
+
+        if thrsholding_type == 'normal':
+            std_no = 1
+
+        mean_diff_thresh = 0.1
+
+        plotting_start, plotting_end = 2637.02,2887.76
+        #end of variables
         
+        well_radius_doi= well_radius_doi.reshape(-1)
+
+    
+        stds_pos, vars_pos, skews_pos, kurts_pos = [], [], [], []
+        stds_neg, vars_neg, skews_neg, kurts_neg = [], [], [], []
+        combined_centroids, final_combined_contour, final_combined_vugs = [], [], []
+        pred_df = pd.DataFrame()
+        c = 0
+        height_idx = 0
+
+
+        
+        
+        contour_x, contour_y = [], []
+        total_filtered_vugs =[]
+        for one_meter_zone_start in tqdm(np.arange(start,end, 1)):
+            one_meter_zone_end = one_meter_zone_start + 1
+            output = get_one_meter_fmi_and_GT(one_meter_zone_start, one_meter_zone_end, 
+                                            tdep_array, fmi_array, well_radius_doi, gt)
+            fmi_array_one_meter_zone, tdep_array_one_meter_zone, well_radius_one_meter_zone, gtZone = output
+            
+            height = fmi_array_one_meter_zone.shape[0]
+
+            different_thresholds = get_mode_of_interest_from_image(fmi_array_one_meter_zone, stride_mode, k)
+            for i, diff_thresh in enumerate(different_thresholds):
+                thresold_img = apply_adaptive_thresholding(fmi_array_one_meter_zone, diff_thresh, block_size = 21, c = c_threshold)
+                holeR, pixLen = well_radius_one_meter_zone.mean()*100, (np.diff(tdep_array_one_meter_zone)*100).mean()
+                    
+                
+                contours, centroids, vugs = get_contours(thresold_img, depth_to=one_meter_zone_start, 
+                                                            depth_from=one_meter_zone_end, radius = holeR, pix_len = pixLen, 
+                                                            min_vug_area = min_vug_area, max_vug_area = max_vug_area, 
+                                                            min_circ_ratio=min_circ_ratio, max_circ_ratio=max_circ_ratio) #values changed here
+                output = get_combined_contours_and_centroids(contours, centroids, vugs,combined_centroids, 
+                                                                final_combined_contour, final_combined_vugs,i, threshold = 5)
+                combined_centroids, final_combined_contour, final_combined_vugs = output
+
+                filtered_contour, filtered_vugs = filter_contours_based_on_original_image(final_combined_contour, final_combined_vugs, 
+                                                                                        fmi_array_one_meter_zone, 0.2)
+                filtered_contour_ = copy.deepcopy(filtered_contour)
+                filtered_vugs_ = copy.deepcopy(filtered_vugs)
+
+                # filter the contours based on the mean pixel in and around the original contour
+                
+                filtered_contour_, filtered_vugs_ = filter_contour_based_on_mean_pixel_in_and_around_original_contour(fmi_array_one_meter_zone, 
+                                                                                                                        filtered_contour_, 
+                                                                                                                        filtered_vugs_, 
+                                                                                                                        threshold = mean_diff_thresh)
+                
+                # total vugs count                                                                                                      
+                total_filtered_vugs.append(filtered_vugs_)
+                for pts in filtered_contour_:
+                    x = pts[:, 0, 0]
+                    y = pts[:, 0, 1]
+                    x = np.append(x, x[0])
+                    y = np.append(y, y[0])
+
+                    y+=height_idx
+
+                    contour_x.append(x)
+                    contour_y.append(y)
+                detected_vugs_percentage = detected_percent_vugs(filtered_contour_, fmi_array_one_meter_zone, tdep_array_one_meter_zone, 
+                                                                one_meter_zone_start, one_meter_zone_end)
+                pred_df = pd.concat([pred_df, detected_vugs_percentage], axis=0)
+            height_idx+=height
+
+        
+               
+        vugs_threshold = 1.5
+        vicinity_threshold = 1
+        per_page_depth_zone = 10 # if selected 6m then change to 6 if 10 then change to 10 1457.8 1498.2 (1498.8)
+        num_rows = (vicinity_threshold*2)+1
+        pred_df_copy = pred_df.reset_index(drop=True)
+
+        for i in range(len(pred_df_copy)-vicinity_threshold):
+            pred_df_zone = pred_df_copy.iloc[i:i+num_rows]
+            idx_voi = list(pred_df_zone.index)[1]
+            if pred_df_copy.iloc[idx_voi].Vugs<=vugs_threshold:
+                if pred_df_zone.Vugs.max()<=vugs_threshold:
+                    pred_df_copy.loc[idx_voi, 'Vugs'] = 0
+
+        fmi_array_doi = fmi_array
+        tdep_array_doi = tdep_array
+
+        img_idx = 0
+        for zone_start in tqdm(np.arange(start, end, per_page_depth_zone)):
+            zone_end = zone_start + per_page_depth_zone
+            zone_start_selected = zone_start
+            temp_mask = (tdep_array_doi>=zone_start) & (tdep_array_doi<=zone_end)
+            fmi_zone = fmi_array_doi[temp_mask]
+            pred_df_zone = pred_df_copy[(pred_df_copy.Depth>=zone_start) & (pred_df_copy.Depth<=zone_end)]
+            gt_zone = gt[(gt.Depth>=zone_start) & (gt.Depth<=zone_end)]
+
+
+            img_height = fmi_zone.shape[0]
+            coord = [[k, j] for i, (k, j) in enumerate(zip(contour_x, contour_y)) if (j.min()>=img_idx) & (j.max()<=(img_idx+img_height))]
+
+            _, ax = plt.subplots(1, 4, figsize=(20, 30), gridspec_kw = {'width_ratios': [3,3,1, 1], 'height_ratios': [1]})
+            ax[0].imshow(fmi_zone, cmap='YlOrBr')
+            ax[1].imshow(fmi_zone, cmap='YlOrBr')
+            for x_, y_ in coord:
+                centroid_y = get_centeroid(np.concatenate([x_.reshape(-1, 1), (y_-img_idx).reshape(-1, 1)], axis=1))[1]
+                scaler = MinMaxScaler((zone_start, zone_end))
+                scaler.fit([[0], [img_height]])
+                centroid_depth = scaler.transform([[centroid_y]])[0][0]
+
+                depth_values, target_value = pred_df_zone.Depth.values, centroid_depth
+
+                # Find the index where the target_value should be inserted
+                insert_index = np.searchsorted(depth_values, target_value, side='right') - 1
+
+                # Check if the target_value is greater than the last depth value, in that case, it will be inserted at the end
+                if insert_index == len(depth_values) - 1 and target_value > depth_values[-1]:
+                    insert_index = len(depth_values) - 1
+                if pred_df_zone.iloc[insert_index].Vugs != 0:
+                    ax[1].plot(x_, y_-img_idx, color='black', linewidth=2)
+
+            ax[1].set_xticks([])
+            ax[1].set_yticks([])
+            ax[0].set_xticks([])
+            ax[0].set_yticks([])
+
+            plot_barh(ax[2], pred_df_zone.Depth.values, pred_df_zone['Vugs'].values, 
+                        zone_start, zone_end-0.1, "Pred\n0-25%", max_scale=25, fontsize = 12)
+                
+            plot_barh(ax[3], gt_zone.Depth.values, gt_zone['Vugs'].values, 
+                        zone_start, zone_end-0.1, "GT\n0-25%", max_scale=25, fontsize = 12)
+            
+            ax[0].set_title("Original FMI", fontsize=12)
+            ax[1].set_title("Contours", fontsize=12)
+
+            plt.tight_layout()
+            st.pyplot(plt)
+            # plt.savefig(f"whole/{zone_start}.png", dpi=400, bbox_inches='tight')
+        #     plt.close()
+            # break
+            img_idx+=img_height
+            # if img_idx>=5000:
+            #     break
+        if st.button('Show Statistical Analysis'):
+            filtered_vugs = [i['area'] for filtered_vugs_ in total_filtered_vugs for i in filtered_vugs_]
+
+            fig1, ax = plt.subplots()
+            sns.histplot(filtered_vugs, ax=ax)
+            st.pyplot(fig1)
 
         
         st.divider()    
@@ -476,40 +678,68 @@ def main():
                 status = "Accepted"
                 zone_start = start
                 zone_end = end
-                # accepted_range = {'start': [start], 'end': [end], 'status': ['Accepted']}
-                accepted_range = pd.DataFrame({'start': [zone_start], 'end': [zone_end], 'status': ['Accepted']})
-                # data = data.concat([data, accepted_range], ignore_index=True)
-                data = pd.concat([data, accepted_range], ignore_index=True)
-            st.divider()
+                # # accepted_range = {'start': [start], 'end': [end], 'status': ['Accepted']}
+                # accepted_range = pd.DataFrame({'start': [zone_start], 'end': [zone_end], 'status': ['Accepted']})
+                # # data = data.concat([data, accepted_range], ignore_index=True)
+                # data = pd.concat([data, accepted_range], ignore_index=True)
+                cursor.execute('''
+                    INSERT INTO ranges (start, end, status)
+                    VALUES (?, ?, ?)
+                ''', (zone_start, zone_end, 'accepted'))
+                conn.commit()
 
             st.markdown("Flag Original Interpretation")
             flag_button = st.button("Flag")
             if flag_button:
+                st.success("Flagged original Interpreta")
                 status = "Flagged"
                 zone_start = start
                 zone_end = end
-                flagged_range = pd.DataFrame({'start': [zone_start], 'end': [zone_end], 'status': ['Flagged']})
-                data = pd.concat([data, flagged_range], ignore_index=True)
-            print(data)
+                # flagged_range = pd.DataFrame({'start': [zone_start], 'end': [zone_end], 'status': ['Flagged']})
+                # data = pd.concat([data, flagged_range], ignore_index=True)
+                cursor.execute('''
+                INSERT INTO ranges (start, end, status)
+                    VALUES (?, ?, ?)
+                ''', (zone_start, zone_end, 'flagged'))
+                conn.commit()
                
             st.markdown("Change parameters for selected depth")
             reiterate_button = st.button("Reiterate")
-        
-        st.divider()   
-        if reiterate_button:
-            button_clicked(reiterate_button, tdep_array, fmi_array, well_radius_doi, gt,start,end,min_vug_area,max_vug_area, min_circ_ratio, max_circ_ratio)
-
-        if not data.empty:
-            st.markdown("Download Accepted and Flagged Ranges")
-
-            csv_data = data.to_csv(index=False)
+            
+            st.markdown("Click here to download report")
+            cursor.execute('SELECT * FROM ranges')
+            rows = cursor.fetchall()
+            csv_data = pd.DataFrame(rows, columns=['start', 'end', 'status']).to_csv(index=False)
             st.download_button(
                 label="Click here",
                 data=csv_data,
                 file_name="accepted_flagged_ranges.csv",
                 key="download_ranges_button"
             )
+                
+        st.divider()   
+        if reiterate_button:
+            button_clicked(reiterate_button, tdep_array, fmi_array, well_radius_doi, gt,start,end,min_vug_area,max_vug_area, min_circ_ratio, max_circ_ratio)
+            cursor.execute('''
+                UPDATE ranges
+                SET status = ?
+                WHERE start = ? AND end = ?
+            ''', ('evaluated', start, end))
+            conn.commit()
 
+        if not data.empty:
+            st.markdown("Download Accepted and Flagged Ranges")
+
+            cursor.execute('SELECT * FROM ranges')
+            rows = cursor.fetchall()
+            csv_data = pd.DataFrame(rows, columns=['start', 'end', 'status']).to_csv(index=False)
+            st.download_button(
+                label="Click here",
+                data=csv_data,
+                file_name="accepted_flagged_report.csv",
+                key="download_ranges_button"
+            )
+        conn.close()
         thresh_percent = 2
 if __name__ == "__main__":
     main()
