c46dc0b82c2502273d2cf9534f0016c19aa58bd1
quamernasim <quamer23nasim@yahoo.com>
2023-10-04 23:56:13 +0530

    new print messege

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def main():
         tdep_array = tdep_array.reshape(-1) 
         well_radius = well_radius.reshape(-1)
 
-        print(fmi_array)
+        print(fmi_array == -9999.0)
 
         fmi_array[fmi_array == -9999.0] = np.nan
         fmi_array = MinMaxScalerCustom(fmi_array, min = 0, max = 255)
