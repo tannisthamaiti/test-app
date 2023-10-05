36a94ce18755d747ef7da8ef9f7f365f241403bf
tannisthamaiti <tannistha.iitkgp@gmail.com>
2023-10-05 11:07:53 -0400

    write st

--- app.py
+++ app.py
@@ -44,9 +44,9 @@ def button_clicked(fmi_array, tdep_array, start, gt,  pred, end):
     tadpoleScaler = MinMaxScaler((0, fmiZone.shape[0]))
     tadpoleScaler.fit([[start], [end]])
     st.markdown("Code working")
-    st.write("idx_end",gtZone)
+    st.write("idx_end",fmiZone[0])
 
-    comparison_plot(fmiZone, tdepZone, start, gtZone,  predZone, end, tadpoleScaler, fmiRatio, fontSize, 3, None, 300, (15, 30), save = False)
+    comparison_plot(fmiZone, tdepZone, start, gtZone,  predZone, end, tadpoleScaler, fmiRatio, fontSize, 3, None, 300, (15, 30), save = True)
 
     
 
