2203e0f4fe58e4bf76e8d2f0a3a2db35192e871d
M Quamer Nasim <quamer23nasim@yahoo.com>
2023-10-11 10:32:48 +0530

    clear whole dir before starting to plot

--- app.py
+++ app.py
@@ -65,9 +65,6 @@ def merge_pdfs(pdf_paths):
 
 
 def main():
-    if os.path.exists('whole'):
-        shutil.rmtree('whole')
-    os.makedirs('whole', exist_ok=True)
     script_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(script_dir)
     # st.set_page_config(page_title="Vug Detection", page_icon="🤖", layout="wide", )  
@@ -108,6 +105,9 @@ def main():
     
     if st.button("Import preloaded DLIS"):
         st.session_state.button_clicked = True
+        if os.path.exists('whole'):
+            shutil.rmtree('whole')
+        os.makedirs('whole', exist_ok=True)
     # if uploaded_file is not None:
     
     if st.session_state.button_clicked:
